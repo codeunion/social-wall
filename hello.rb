58b5454e102439f53267696c2b197503680af165
codeunion <accounts@codeunion.io>
2014-09-05 23:20:37 +0000

    Add clearer code to hello.rb

--- hello.rb
+++ hello.rb
@@ -2,7 +2,11 @@ require 'sinatra'     # Load the Sinatra web framework
 
 get("/") do
   html = "Hello, world!"
-
+  html.concat("<div><a href='/waffles'>show me waffles</a></div>")
+  html.concat("<div><a href='/waffles/chocolate'>show me chocolate</a></div>")
+  html.concat("<div><a href='/bake?baked_good=cookies&count=10'>bake 10 cookies</a></div>")
+  html.concat("<div><a href='/bake?baked_good=cronut&count=5'>bake 5 cronuts</a></div>")
+  html.concat("<div><a href='/bake?baked_good=cupcakes&count=1138'>bake 1138 cupcakes</a></div>")
   body(html)
 end
 
@@ -28,7 +32,7 @@ get("/bake") do
 
   html.concat("<ul>")
   1.upto(count) do |num|
-    html.concat("<li>Waffle number #{num}</li>")
+    html.concat("<li>#{baked_good} number #{num}</li>")
   end
   html.concat("</ul>")
 
